bdec6f1cdc98cea31d988f7cf1072c4820b1efe2
liaoxianfu <liaoxianfu555@outlook.com>
2018-10-23 19:58:36 +0800

    添加机构与教师模型

---
+++ organization/models.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+from django.db import models
+
+
+# Create your models here.
+
+class CityDict(models.Model):
+    name = models.CharField(max_length=20, verbose_name=u"城市")
+    # 城市描述：备用不一定展示出来
+    desc = models.CharField(max_length=200, verbose_name=u"描述")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"城市"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+# 课程机构
+class CourseOrg(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u"机构名称")
+    # 机构描述，后面会替换为富文本展示
+    desc = models.TextField(verbose_name=u"机构描述")
+    click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
+    fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
+    image = models.ImageField(
+        upload_to="org/%Y/%m",
+        verbose_name=u"封面图",
+        max_length=100)
+    address = models.CharField(max_length=150, verbose_name=u"机构地址")
+    # 一个城市可以有很多课程机构，通过将city设置外键，变成课程机构的一个字段
+    # 可以让我们通过机构找到城市
+    city = models.ForeignKey(CityDict, verbose_name=u"所在城市")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"课程机构"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+# 教师
+class Teacher(models.Model):
+    org = models.ForeignKey(CourseOrg, verbose_name=u"所属机构")
+    name = models.CharField(max_length=50, verbose_name=u"教师名称")
+    work_years = models.IntegerField(default=0, verbose_name=u"工作年限")
+    work_company = models.CharField(max_length=50, verbose_name=u"就职学校")
+    work_position = models.CharField(max_length=50, verbose_name=u"职称")
+    points = models.CharField(max_length=50, verbose_name=u"教学特点")
+    click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
+    fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"教师"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
