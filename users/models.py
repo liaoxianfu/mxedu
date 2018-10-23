7792571d5412e5ba5aaf25d4d7036fd27624ade2
liaoxianfu <liaoxianfu555@outlook.com>
2018-10-23 18:02:33 +0800

    Initial commit 创建user models

---
+++ users/models.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class UserProfile(AbstractUser):
+    GENDER_CHOICES = (
+        ('male', u'男'),
+        ('female', u'女')
+    )
+
+    # 昵称
+    nike_name = models.CharField(max_length=150, verbose_name=u'昵称', default="")
+    # 生日
+    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    # 性别 只能选择男或者女
+    gender = models.CharField(
+        max_length=5,
+        verbose_name=u"性别",
+        choices=GENDER_CHOICES,
+        default="female")
+    # 地址
+    address = models.CharField(max_length=100, verbose_name="地址", default="")
+    # 电话
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    # 头像 默认使用default.png
+    image = models.ImageField(
+        upload_to="image/%Y/%m",
+        default=u"image/default.png",
+        max_length=100
+    )
+
+    class Meta:
+        verbose_name = u"用户信息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    SEND_CHOICES = (
+        ("register", u"注册"),
+        ("forget", u"找回密码")
+    )
+    code = models.CharField(max_length=20, verbose_name=u"验证码")
+    # 未设置null = true blank = true 默认不可为空
+    email = models.EmailField(max_length=50, verbose_name=u"邮箱")
+    send_type = models.CharField(choices=SEND_CHOICES, max_length=10)
+    # 这里的now得去掉(),不去掉会根据编译时间。而不是根据实例化时间。
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = "邮箱验证码"
+        verbose_name_plural = verbose_name
