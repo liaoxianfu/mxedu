17b6457eca0e89a070bb8ba067ff17b7b17903a9
liaoxianfu <liaoxianfu555@outlook.com>
2018-10-23 21:53:24 +0800

    xadmin 导入并设计model

---
+++ apps/users/adminx.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+__author__ = 'liaoxianfu'
+__date__ = '2018/10/23 21:27'
+
+import xadmin
+
+from .models import EmailVerifyRecord, Banner
+
+
+class EmailVerifyRecordAdmin(object):
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    # 配置搜索字段,不做时间搜索
+    search_fields = ['code', 'email', 'send_type']
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+xadmin.site.register(Banner, BannerAdmin)
--- apps/users/models.py
+++ apps/users/models.py
@@ -17,10 +17,10 @@ class UserProfile(AbstractUser):
     birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
     # 性别 只能选择男或者女
     gender = models.CharField(
-        max_length=5,
+        max_length=10,
         verbose_name=u"性别",
         choices=GENDER_CHOICES,
-        default="female")
+        default="male")
     # 地址
     address = models.CharField(max_length=100, verbose_name="地址", default="")
     # 电话
@@ -51,14 +51,15 @@ class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=20, verbose_name=u"验证码")
     # 未设置null = true blank = true 默认不可为空
     email = models.EmailField(max_length=50, verbose_name=u"邮箱")
-    send_type = models.CharField(choices=SEND_CHOICES, max_length=10)
+    send_type = models.CharField(choices=SEND_CHOICES, max_length=10,verbose_name=u'发送类型')
     # 这里的now得去掉(),不去掉会根据编译时间。而不是根据实例化时间。
-    send_time = models.DateTimeField(default=datetime.now)
+    send_time = models.DateTimeField(default=datetime.now,verbose_name=u'发送时间')
 
     class Meta:
         verbose_name = "邮箱验证码"
         verbose_name_plural = verbose_name
-
+    def __str__(self):
+        return '{0}({1})'.format(self.code,self.email)
 
 class Banner(models.Model):
     """
