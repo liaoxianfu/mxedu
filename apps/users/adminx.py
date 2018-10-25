30d3fdffca5294d41acb4dafb8dfffe56874a1f6
liaoxianfu <liaoxianfu555@outlook.com>
2018-10-25 18:03:27 +0800

    修改后台的列表

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -3,14 +3,17 @@ __author__ = 'liaoxianfu'
 __date__ = '2018/10/23 21:27'
 
 import xadmin
-
-from .models import EmailVerifyRecord, Banner
+from xadmin import views
+from courses.models import *
+from operation.models import *
+from organization.models import *
+from users.models import *
+from xadmin.models import Log
+from django.contrib.auth.models import Group, Permission
 
 """
 xadmin 的全局配置
 """
-
-from xadmin import views
 
 
 # 创建X admin的全局管理器并与view绑定。
@@ -20,10 +23,6 @@ class BaseSetting(object):
     use_bootswatch = True
 
 
-# 将全局配置管理与view绑定注册
-xadmin.site.register(views.BaseAdminView, BaseSetting)
-
-
 # x admin 全局配置参数信息设置
 class GlobalSettings(object):
     site_title = "过不去的过去: 后台管理"
@@ -31,9 +30,36 @@ class GlobalSettings(object):
     # 收起菜单
     menu_style = "accordion"
 
+    def get_site_menu(self):
+        return (
+            {'title': '课程管理', 'menus': (
+                {'title': '课程信息', 'url': self.get_model_url(Course, 'changelist')},
+                {'title': '章节信息', 'url': self.get_model_url(Lesson, 'changelist')},
+                {'title': '视频信息', 'url': self.get_model_url(Video, 'changelist')},
+                {'title': '课程资源', 'url': self.get_model_url(CourseResource, 'changelist')},
+                {'title': '课程评论', 'url': self.get_model_url(CourseComments, 'changelist')},
+            )},
+            {'title': '机构管理', 'menus': (
+                {'title': '所在城市', 'url': self.get_model_url(CityDict, 'changelist')},
+                {'title': '机构讲师', 'url': self.get_model_url(Teacher, 'changelist')},
+                {'title': '机构信息', 'url': self.get_model_url(CourseOrg, 'changelist')},
+            )},
+            {'title': '用户管理', 'menus': (
+                {'title': '用户信息', 'url': self.get_model_url(UserProfile, 'changelist')},
+                {'title': '用户验证', 'url': self.get_model_url(EmailVerifyRecord, 'changelist')},
+                {'title': '用户课程', 'url': self.get_model_url(UserCourse, 'changelist')},
+                {'title': '用户收藏', 'url': self.get_model_url(UserFavorite, 'changelist')},
+                {'title': '用户消息', 'url': self.get_model_url(UserMessage, 'changelist')},
+            )},
 
-# 将头部与脚部信息进行注册:
-xadmin.site.register(views.CommAdminView, GlobalSettings)
+            {'title': '系统管理', 'menus': (
+                {'title': '用户咨询', 'url': self.get_model_url(UserAsk, 'changelist')},
+                {'title': '首页轮播', 'url': self.get_model_url(Banner, 'changelist')},
+                {'title': '用户分组', 'url': self.get_model_url(Group, 'changelist')},
+                {'title': '用户权限', 'url': self.get_model_url(Permission, 'changelist')},
+                {'title': '日志记录', 'url': self.get_model_url(Log, 'changelist')},
+            )},
+        )
 
 
 class EmailVerifyRecordAdmin(object):
@@ -42,13 +68,13 @@ class EmailVerifyRecordAdmin(object):
     search_fields = ['code', 'email', 'send_type']
 
 
-xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
-
-
 class BannerAdmin(object):
     list_display = ['title', 'image', 'url', 'index', 'add_time']
     search_fields = ['title', 'image', 'url', 'index']
     list_filter = ['title', 'image', 'url', 'index', 'add_time']
 
 
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
